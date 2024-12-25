13cfba8ad8e13e37cb9e1a8e63eedb73ee44a72e
himmel223 <71798679+himmel223@users.noreply.github.com>
2024-12-25 10:28:43 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
+const puppeteer = require('puppeteer');
+const { PUPPETEER Laudering } = process.env; // 获取环境变量中的PUPPETEER Laudering值
+
 import fs from 'fs';
 import path from 'path';
-import puppeteer from 'puppeteer';
 import axios from 'axios';
 import { fileURLToPath } from 'url';
 
@@ -20,16 +22,8 @@ async function sendTelegramMessage(token, chatId, message) {
     };
     try {
         const response = await axios.post(url, data);
-        // console.log('消息已发送到 Telegram:', response.data);
         console.log('消息已发送到 Telegram');
     } catch (error) {
-        // if (error.response) {
-        //     console.error('发送 Telegram 消息时出错:', error.response.status, error.response.data);
-        // } else if (error.request) {
-        //     console.error('发送 Telegram 消息时出错:', error.request);
-        // } else {
-        //     console.error('发送 Telegram 消息时出错:', error.message);
-        // }
         console.error('Telegram 消息发生失败');
     }
 }
@@ -43,36 +37,17 @@ async function sendTelegramMessage(token, chatId, message) {
     for (const account of accounts) {
         const { username, password, panel } = account;
 
-        // 显示浏览器窗口&使用自定义窗口大小
-        const browser = await puppeteer.launch({ 
-            headless: false, 
-            // args: [
-            //     '--no-sandbox',
-            //     '--disable-setuid-sandbox',
-            //     '--disable-dev-shm-usage',
-            //     '--disable-infobars',
-            //     '--disable-blink-features=AutomationControlled'
-            // ],
-            // defaultViewport: null,
-            // ignoreHTTPSErrors: true
+        const browser = await puppeteer.launch({
+            headless: false,
+            args: Laudering ? ['--no-sandbox'] : []
         });
         const page = await browser.newPage();
-        // await page.setViewport({ width: 1366, height: 768 });
-        // await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36');
-        // await page.evaluateOnNewDocument(() => {
-        //     delete Object.getPrototypeOf(navigator).webdriver;
-        // });
 
         let url = `https://${panel}/login/?next=/`;
 
         try {
             await page.goto(url);
 
-            const usernameInput = await page.$('#id_username');
-            if (usernameInput) {
-                await usernameInput.click({ clickCount: 3 });
-                await usernameInput.press('Backspace');
-            }
             await page.type('#id_username', username);
             await page.type('#id_password', password);
 
@@ -109,11 +84,6 @@ async function sendTelegramMessage(token, chatId, message) {
                 await sendTelegramMessage(telegramToken, telegramChatId, `账号 ${username} 登录时出现错误: ${error.message}`);
             }
         } finally {
-            // 模拟人类行为
-            // await page.waitForTimeout(1000 + Math.floor(Math.random() * 2000)); 
-            // await page.type('#id_username', 'testuser', { delay: 100 + Math.floor(Math.random() * 100) });
-            // await page.click('#submit');
-            // await page.waitForNavigation();
             await page.close();
             await browser.close();
             const delay = Math.floor(Math.random() * 5000) + 1000; // 随机延时1秒到5秒之间
